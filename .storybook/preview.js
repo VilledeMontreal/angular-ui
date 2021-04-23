7b1fbff9950c1950e6a8dd20574e725ed7613087
Yann Debonnel <yann.debonnel@montreal.ca>
2021-04-23 12:59:27 -0400

    feat: add a global.scss file that would include non component specific rule

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,4 @@
+import '!style-loader!css-loader!../dist/core-components-angular-lib/global.scss';
 import '!style-loader!css-loader!./fix-code-block-in-preview.css';
 import { withA11y } from '@storybook/addon-a11y';
 import { setCompodocJson } from '@storybook/addon-docs/angular';

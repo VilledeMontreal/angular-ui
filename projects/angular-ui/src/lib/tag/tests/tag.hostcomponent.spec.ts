c5a9a491fd9979dbe98327bd83f1fde94636f2cd
Maude LAFLAMME <maude.laflamme@montreal.ca>
2022-03-08 13:24:28 -0500

    test(tag): add unit tests for BaoTagComponent
    Signed-off-by: Maude LAFLAMME <maude.laflamme@montreal.ca>
    
    remove warning from tag component. close #20
    
    Signed-off-by: Maude LAFLAMME <maude.laflamme@montreal.ca>

---
+++ projects/angular-ui/src/lib/tag/tests/tag.hostcomponent.spec.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2022 Ville de Montreal. All rights reserved.
+ * Licensed under the MIT license.
+ * See LICENSE file in the project root for full license information.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  template: `
+    <bao-tag [type]="type" [hiddenText]="hiddenText" [variant]="variant">
+      <span>Label</span>
+    </bao-tag>
+  `
+})
+export class TestTagHostComponent {
+  type: string;
+  hiddenText: string;
+  variant: string;
+}
+
+@Component({
+  template: `
+    <bao-tag [type]="type" [hiddenText]="hiddenText" [variant]="variant">
+      <bao-icon svgIcon="icon-warning"></bao-icon>
+      <span>Label</span>
+    </bao-tag>
+  `
+})
+export class TestTagWithIconHostComponent {
+  type: string;
+  hiddenText: string;
+  variant: string;
+}

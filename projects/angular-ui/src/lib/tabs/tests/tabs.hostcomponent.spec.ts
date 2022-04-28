70bafe399a120ca26b9de0dfeb1b88693d9f784c
MaudeLaflamme <33531625+MaudeLaflamme@users.noreply.github.com>
2022-04-28 09:13:27 -0400

    feat(tabs): add bao-tabs component. close #51 (#62)
    Signed-off-by: Maude LAFLAMME <maude.laflamme@montreal.ca>

---
+++ projects/angular-ui/src/lib/tabs/tests/tabs.hostcomponent.spec.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2022 Ville de Montreal. All rights reserved.
+ * Licensed under the MIT license.
+ * See LICENSE file in the project root for full license information.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  template: ` <bao-tabs>
+    <bao-tablist [size]="size">
+      <button bao-tab-header>Tab1</button>
+      <button bao-tab-header>Tab2</button>
+      <button bao-tab-header disabled>Tab3</button>
+    </bao-tablist>
+    <bao-panel><p>Panel1</p></bao-panel>
+    <bao-panel><p>Panel2</p></bao-panel>
+    <bao-panel><p>Panel3</p></bao-panel>
+  </bao-tabs>`
+})
+export class TestTablistHostComponent {
+  size: string;
+}
---
+++ projects/storybook-angular/src/stories/Tabs/Tabs.stories.ts
@@ -0,0 +1,203 @@
+/*
+ * Copyright (c) 2022 Ville de Montreal. All rights reserved.
+ * Licensed under the MIT license.
+ * See LICENSE file in the project root for full license information.
+ */
+import { CommonModule } from '@angular/common';
+import { componentWrapperDecorator, moduleMetadata } from '@storybook/angular';
+import { Meta, Story } from '@storybook/angular/types-6-0';
+import {
+  BaoTablistComponent,
+  BaoTabHeader,
+  BaoTabPanel,
+  BaoTabsContainer
+} from 'angular-ui';
+
+const description = `
+The Tabs component allows to navigate quickly between different views from a same object. `;
+
+export default {
+  title: 'Components/Tabs',
+  decorators: [
+    moduleMetadata({
+      declarations: [BaoTabHeader, BaoTabPanel, BaoTabsContainer],
+      imports: [CommonModule]
+    }),
+    componentWrapperDecorator(
+      story => `<div style="max-width:30rem;">${story}</div>`
+    )
+  ],
+  component: BaoTablistComponent,
+  parameters: {
+    docs: {
+      description: {
+        component: description
+      }
+    }
+  },
+  argTypes: {
+    ngAfterViewInit: {
+      table: {
+        disable: true
+      }
+    },
+    setInitialActiveTab: {
+      table: {
+        disable: true
+      }
+    },
+    setTablistAttributes: {
+      table: {
+        disable: true
+      }
+    },
+    activeTabIndex: {
+      table: {
+        disable: true
+      }
+    },
+    uniqueId: {
+      table: {
+        disable: true
+      }
+    },
+    changeActiveTab: {
+      table: {
+        disable: true
+      }
+    },
+    leftKeyEvent: {
+      table: {
+        disable: true
+      }
+    },
+    rightKeyEvent: {
+      table: {
+        disable: true
+      }
+    },
+    ngAfterContentInit: {
+      table: {
+        disable: true
+      }
+    },
+    ngOnDestroy: {
+      table: {
+        disable: true
+      }
+    },
+    tabHeaders: {
+      table: {
+        disable: true
+      }
+    },
+    activeTabChange: {
+      table: {
+        disable: true
+      }
+    },
+    getNextActivableTabIndex: {
+      table: {
+        disable: true
+      }
+    },
+    tabKeyEvent: {
+      table: {
+        disable: true
+      }
+    },
+    tabShiftKeyEvent: {
+      table: {
+        disable: true
+      }
+    }
+  }
+} as Meta;
+
+const Template: Story<BaoTablistComponent> = (args: BaoTablistComponent) => ({
+  component: BaoTablistComponent,
+  template: `
+      <bao-tabs>
+        <bao-tablist [ariaLabel]="ariaLabel" [size]="size">
+          <button bao-tab-header>
+            Tab #1
+          </button>
+          <button bao-tab-header>
+            Tab #2
+          </button>
+          <button bao-tab-header disabled>
+            Tab #3 (disabled)
+          </button>
+        </bao-tablist>
+        <bao-panel><p>This is the content for tab #1.</p></bao-panel>
+        <bao-panel><p>And this is the content for tab #2.</p></bao-panel>
+        <bao-panel><p>This content is undisplayable.</p></bao-panel>
+      </bao-tabs>
+   `,
+  props: args
+});
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  ariaLabel: 'onglets',
+  size: 'large'
+};
+
+export const TabsSize: Story = args => ({
+  props: args,
+  template: `
+      <bao-tabs>
+        <bao-tablist size="large" style="margin-bottom:2rem;">
+          <button bao-tab-header>
+            Tab #1
+          </button>
+          <button bao-tab-header>
+            Tab #2
+          </button>
+          <button bao-tab-header>
+            Tab #3
+          </button>
+        </bao-tablist>
+        <bao-panel></bao-panel>
+        <bao-panel></bao-panel>
+        <bao-panel></bao-panel>
+      </bao-tabs>
+      <bao-tabs>
+        <bao-tablist size="medium" style="margin-bottom:2rem;">
+          <button bao-tab-header>
+            Tab #1
+          </button>
+          <button bao-tab-header>
+            Tab #2
+          </button>
+          <button bao-tab-header>
+            Tab #3
+          </button>
+        </bao-tablist>
+        <bao-panel></bao-panel>
+        <bao-panel></bao-panel>
+        <bao-panel></bao-panel>
+      </bao-tabs>
+      <bao-tabs>
+        <bao-tablist size="small">
+          <button bao-tab-header>
+            Tab #1
+          </button>
+          <button bao-tab-header>
+            Tab #2
+          </button>
+          <button bao-tab-header>
+            Tab #3
+          </button>
+        </bao-tablist>
+        <bao-panel></bao-panel>
+        <bao-panel></bao-panel>
+        <bao-panel></bao-panel>
+      </bao-tabs>
+      `
+});
+TabsSize.storyName = 'Sizes';
+TabsSize.args = {
+  ...Primary.args
+};

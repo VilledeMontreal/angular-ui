a43dc66562aaf9ec8c76f3be66ba8bd61fc8be12
Maude LAFLAMME <maude.laflamme@montreal.ca>
2023-01-18 16:48:21 -0500

    fix(file): remove unnecessary margin and fix error when no guiding test is provided. close #117.
    Signed-off-by: Maude LAFLAMME <maude.laflamme@montreal.ca>

--- projects/angular-ui/src/lib/file/file-input.component.ts
+++ projects/angular-ui/src/lib/file/file-input.component.ts
@@ -273,10 +273,14 @@ export class BaoFileInputComponent
   private setDescribedByAttribute(): void {
     const helperText = Array.from(this.nativeElement.children).find(
       (el: HTMLElement) => el.localName === 'bao-guiding-text'
-    ).firstElementChild;
+    );
     if (helperText) {
       this._helperTextId = `bao-guiding-text-${fileTextUniqueId++}`;
-      this.renderer.setAttribute(helperText, 'id', this._helperTextId);
+      this.renderer.setAttribute(
+        helperText.firstElementChild,
+        'id',
+        this._helperTextId
+      );
       const inputElement = Array.from(this.nativeElement.children)
         .find((el: HTMLElement) => el.className == 'file-drop-zone')
         .children.item(1);

8cfbe14fb0fcf6b04843be804d9625086f703799
mtlmatt <103215614+mtlmatt@users.noreply.github.com>
2023-03-21 10:23:40 -0400

    feat: #54 reintroduce the snackbar (#151)
    * feat: #54 reintroduce the snackbar
    
    Signed-off-by: Matthieu <matthieu.perrin@montreal.ca>
    
    * fix: code review changes
    
    Signed-off-by: Matthieu <matthieu.perrin@montreal.ca>

---
+++ projects/storybook-angular-examples/src/app/snack-bar/snack-bar-simple-with-config/snack-bar-simple-with-config-example.component.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2023 Ville de Montreal. All rights reserved.
+ * Licensed under the MIT license.
+ * See LICENSE file in the project root for full license information.
+ */
+import { Component } from '@angular/core';
+import { BaoSnackBarService } from 'projects/angular-ui/src/public-api';
+
+/**
+ * @title Opens a snackbar with extra configuration.
+ */
+@Component({
+  selector: 'snack-bar-simple-with-config-example',
+  templateUrl: 'snack-bar-simple-with-config-example.html'
+})
+export class SnackBarSimpleWithConfigExample {
+  constructor(public snackBarService: BaoSnackBarService) {}
+
+  public openCenterDisplay() {
+    this.snackBarService.open({
+      message: 'Center display',
+      showClose: true,
+      horizontalPosition: 'center'
+    });
+  }
+
+  public openRightDisplay() {
+    this.snackBarService.open({
+      message: 'Right display',
+      showClose: true,
+      horizontalPosition: 'right'
+    });
+  }
+
+  public open10sDuration() {
+    this.snackBarService.open({
+      message: '10s duration',
+      duration: 10000
+    });
+  }
+}

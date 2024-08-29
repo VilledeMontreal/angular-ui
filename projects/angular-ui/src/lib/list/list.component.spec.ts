ed89dc342a47c9c7afd9d001419b1f0370c168d1
TolaSam <74562315+TolaSam@users.noreply.github.com>
2024-08-29 16:08:19 -0400

    feat: crmmon-7566 - Add a dropdown menu as a list item option (#214)
    Signed-off-by: Tola <tola.sam@montreal.ca>
    Signed-off-by: Maude Laflamme <maude.laflamme@montreal.ca>

---
+++ projects/angular-ui/src/lib/list/list.component.spec.ts
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2024 Ville de Montreal. All rights reserved.
+ * Licensed under the MIT license.
+ * See LICENSE file in the project root for full license information.
+ */
+import { OverlayModule } from '@angular/cdk/overlay';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { BaoListModule } from '.';
+import { BaoButtonModule } from '../button';
+import { BaoDropdownMenuModule } from '../dropdown-menu';
+import { BaoIconModule } from '../icon';
+import { TestListDropdownMenuComponent } from './tests/list.hostcomponent.spec';
+
+describe('BaoList', () => {
+  describe('TestListDropdownMenuComponent', () => {
+    let fixture: ComponentFixture<TestListDropdownMenuComponent>;
+    let TestListDropdownMenuComponentElement: DebugElement;
+
+    beforeEach(waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [TestListDropdownMenuComponent],
+        imports: [
+          OverlayModule,
+          BaoListModule,
+          BaoDropdownMenuModule,
+          BaoButtonModule,
+          BaoIconModule
+        ]
+      });
+      return TestBed.compileComponents();
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(TestListDropdownMenuComponent);
+      TestListDropdownMenuComponentElement = fixture.debugElement;
+    });
+
+    it('should have a list', () => {
+      const element = TestListDropdownMenuComponentElement.queryAll(
+        By.css('.bao-list')
+      );
+
+      expect(element).toBeTruthy();
+    });
+
+    it('should have an icon', () => {
+      const element = TestListDropdownMenuComponentElement.queryAll(
+        By.css('.bao-list-item-header .bao-icon')
+      );
+
+      expect(element).toBeTruthy();
+    });
+
+    it('should have a title', () => {
+      const element = TestListDropdownMenuComponentElement.queryAll(
+        By.css(
+          '.bao-list-item-content .bao-list-item-text .bao-list-item-title'
+        )
+      );
+
+      expect(element).toBeTruthy();
+    });
+
+    it('should have a description', () => {
+      const element = TestListDropdownMenuComponentElement.queryAll(
+        By.css(
+          '.bao-list-item-content .bao-list-item-text .bao-list-item-description li'
+        )
+      );
+
+      expect(element).toBeTruthy();
+    });
+
+    it('should have a tag', () => {
+      const element = TestListDropdownMenuComponentElement.queryAll(
+        By.css('.bao-list-item-content .bao-list-item-tag .bao-tag')
+      );
+
+      expect(element).toBeTruthy();
+    });
+
+    it('should have a dropdown menu within the list item', () => {
+      const element = TestListDropdownMenuComponentElement.queryAll(
+        By.css('.bao-list-item .bao-dropdown-menu-container')
+      );
+
+      expect(element).toBeTruthy();
+    });
+
+    it('should have as many dropdown menus as the list items', () => {
+      const listItemElement = TestListDropdownMenuComponentElement.queryAll(
+        By.css('.bao-list-item')
+      );
+      const dropDownMenuElement = TestListDropdownMenuComponentElement.queryAll(
+        By.css('.bao-dropdown-menu-container')
+      );
+
+      expect(listItemElement.length).toEqual(dropDownMenuElement.length);
+    });
+  });
+});
--- projects/storybook-angular/src/stories/List/List.stories.ts
+++ projects/storybook-angular/src/stories/List/List.stories.ts
@@ -3,12 +3,20 @@
  * Licensed under the MIT license.
  * See LICENSE file in the project root for full license information.
  */
+import { OverlayModule } from '@angular/cdk/overlay';
+import { PortalModule } from '@angular/cdk/portal';
+import { CommonModule } from '@angular/common';
 import { moduleMetadata } from '@storybook/angular';
 import { Meta, Story } from '@storybook/angular/types-6-0';
 import {
+  BaoAvatarModule,
+  BaoButtonModule,
+  BaoCheckboxModule,
+  BaoDropdownMenuModule,
   BaoIconModule,
   BaoListItem,
   BaoListModule,
+  BaoRadioModule,
   BaoTagModule
 } from 'angular-ui';
 
@@ -23,7 +31,19 @@ export default {
   decorators: [
     moduleMetadata({
       // declarations: [BaoListItem],
-      imports: [BaoListModule, BaoIconModule, BaoTagModule]
+      imports: [
+        BaoListModule,
+        BaoIconModule,
+        BaoTagModule,
+        CommonModule,
+        BaoDropdownMenuModule,
+        BaoButtonModule,
+        BaoAvatarModule,
+        BaoCheckboxModule,
+        BaoRadioModule,
+        OverlayModule,
+        PortalModule
+      ]
     })
   ],
   component: BaoListItem,
@@ -128,7 +148,7 @@ export const SimpleListWithTagAndIcon: Story = args => ({
     <bao-list-item>
         <bao-icon baoIconItemType svgIcon="icon-eye"></bao-icon>
         <span bao-list-item-title>Title</span>
-        <bao-tag type="positive"><span>Label</span></bao-tag>
+        <bao-tag type="alert"><bao-icon svgIcon="icon-warning"></bao-icon><span>Non vérifié</span></bao-tag>
     </bao-list-item>
   </bao-list>
   `
@@ -163,7 +183,7 @@ export const SimpleListWithDescription: Story = args => ({
     <bao-list-item>
       <bao-icon baoIconItemType svgIcon="icon-eye"></bao-icon>
       <span bao-list-item-title>Title</span>
-      <bao-tag type="positive"><span>Label</span></bao-tag>
+      <bao-tag type="alert"><bao-icon svgIcon="icon-warning"></bao-icon><span>Non vérifié</span></bao-tag>
       <bao-list-item-description>
         <div>Description 1</div>
         <div>Description 2</div>
@@ -206,12 +226,175 @@ export const SimpleListWithInlineDescription: Story = args => ({
         <li>Description 1</li>
         <li>Description 2</li>
       </ul>
-      <bao-tag type="positive"><span>Label</span></bao-tag>
+      <bao-tag type="alert"><bao-icon svgIcon="icon-warning"></bao-icon><span>Non vérifié</span></bao-tag>
     </bao-list-item>
   </bao-list>
   `
 });
 SimpleListWithInlineDescription.storyName = 'Simple list - Inline Description';
 SimpleListWithInlineDescription.args = {
+  ...Primary.args
+};
+
+export const SimpleListWithDropdownMenu: Story = args => ({
+  props: args,
+  template: `
+    <bao-list>
+      <bao-list-item>
+          <bao-icon baoIconItemType svgIcon="icon-eye"></bao-icon>
+          <span bao-list-item-title>Title</span>
+          <ul bao-list-item-description>
+            <li>Description 1</li>
+            <li>Description 2</li>
+          </ul>
+          <bao-tag type="positive"><span>Label</span></bao-tag>
+          <button bao-button [baoDropdownMenuTriggerFor]="menu1" type="editorial" level="tertiary" 
+            size="medium"  aria-label="Cliquer pour voir les actions">
+            <bao-icon svgIcon="icon-more-vertical"></bao-icon>
+          </button>
+          <bao-dropdown-menu #menu1>
+            <ul>
+                <li>
+                  <a bao-dropdown-menu-item>
+                      <bao-dropdown-menu-item-label>Libellé 1A</bao-dropdown-menu-item-label>
+                  </a>
+                </li>
+                <li>
+                  <a bao-dropdown-menu-item>
+                      <bao-dropdown-menu-item-label>Libellé 1B</bao-dropdown-menu-item-label>
+                  </a>
+                </li>
+                <li>
+                  <a bao-dropdown-menu-item>
+                      <bao-dropdown-menu-item-label>Libellé 1C</bao-dropdown-menu-item-label>
+                  </a>
+                </li>
+            </ul>
+          </bao-dropdown-menu>
+      </bao-list-item>
+      <bao-list-item>
+          <bao-icon baoIconItemType svgIcon="icon-eye"></bao-icon>
+          <span bao-list-item-title>Title</span>
+          <ul bao-list-item-description>
+            <li>Description 1</li>
+            <li>Description 2</li>
+          </ul>
+          <bao-tag type="alert">
+            <bao-icon svgIcon="icon-warning"></bao-icon>
+            <span>Non vérifié</span>
+          </bao-tag>
+          <button bao-button [baoDropdownMenuTriggerFor]="menu2" type="editorial" level="tertiary" 
+            size="medium" aria-label="Cliquer pour voir les actions">
+            <bao-icon svgIcon="icon-more-vertical"></bao-icon>
+          </button>
+          <bao-dropdown-menu #menu2>
+            <ul>
+                <li>
+                  <a bao-dropdown-menu-item>
+                      <bao-dropdown-menu-item-label>Libellé 2A</bao-dropdown-menu-item-label>
+                  </a>
+                </li>
+                <li>
+                  <a bao-dropdown-menu-item>
+                      <bao-dropdown-menu-item-label>Libellé 2B</bao-dropdown-menu-item-label>
+                  </a>
+                </li>
+                <li>
+                  <a bao-dropdown-menu-item>
+                      <bao-dropdown-menu-item-label>Libellé 2C</bao-dropdown-menu-item-label>
+                  </a>
+                </li>
+            </ul>
+          </bao-dropdown-menu>
+      </bao-list-item>
+      <bao-list-item>
+          <bao-icon baoIconItemType svgIcon="icon-eye"></bao-icon>
+          <span bao-list-item-title>Title</span>
+          <ul bao-list-item-description>
+            <li>Description 1</li>
+            <li>Description 2</li>
+          </ul>
+          <button bao-button [baoDropdownMenuTriggerFor]="menu3" type="editorial" level="tertiary" 
+            size="medium" aria-label="Cliquer pour voir les actions">
+            <bao-icon svgIcon="icon-more-vertical"></bao-icon>
+          </button>
+          <bao-dropdown-menu #menu3>
+            <ul>
+                <li>
+                  <a bao-dropdown-menu-item>
+                      <bao-dropdown-menu-item-label>Libellé 3A</bao-dropdown-menu-item-label>
+                  </a>
+                </li>
+                <li>
+                  <a bao-dropdown-menu-item>
+                      <bao-dropdown-menu-item-label>Libellé 3B</bao-dropdown-menu-item-label>
+                  </a>
+                </li>
+                <li>
+                  <a bao-dropdown-menu-item>
+                      <bao-dropdown-menu-item-label>Libellé 3C</bao-dropdown-menu-item-label>
+                  </a>
+                </li>
+            </ul>
+          </bao-dropdown-menu>
+      </bao-list-item>
+    </bao-list>
+  `
+});
+SimpleListWithDropdownMenu.storyName = 'Simple list - Dropdown menu';
+SimpleListWithDropdownMenu.args = {
+  ...Primary.args
+};
+
+export const SimpleListWithActionButton: Story = args => ({
+  props: args,
+  template: `
+    <bao-list>
+      <bao-list-item>
+          <bao-icon baoIconItemType svgIcon="icon-eye"></bao-icon>
+          <span bao-list-item-title>Title</span>
+          <ul bao-list-item-description>
+            <li>Description 1</li>
+            <li>Description 2</li>
+          </ul>
+          <bao-tag type="positive"><span>Label</span></bao-tag>
+          <button bao-button type="editorial" level="tertiary" 
+            size="medium" aria-label="Supprimer cet élément">
+            <bao-icon svgIcon="icon-trash"></bao-icon>
+          </button>
+      </bao-list-item>
+      <bao-list-item>
+          <bao-icon baoIconItemType svgIcon="icon-eye"></bao-icon>
+          <span bao-list-item-title>Title</span>
+          <ul bao-list-item-description>
+            <li>Description 1</li>
+            <li>Description 2</li>
+          </ul>
+          <bao-tag type="alert">
+            <bao-icon svgIcon="icon-warning"></bao-icon>
+            <span>Non vérifié</span>
+          </bao-tag>
+          <button bao-button type="editorial" level="tertiary" 
+            size="medium" aria-label="Éditer cet élément">
+            <bao-icon svgIcon="icon-edit"></bao-icon>
+          </button>
+      </bao-list-item>
+      <bao-list-item>
+          <bao-icon baoIconItemType svgIcon="icon-eye"></bao-icon>
+          <span bao-list-item-title>Title</span>
+          <ul bao-list-item-description>
+            <li>Description 1</li>
+            <li>Description 2</li>
+          </ul>
+          <button bao-button type="editorial" level="tertiary" 
+            size="medium" aria-label="Éditer cet élément">
+            <bao-icon svgIcon="icon-edit"></bao-icon>
+          </button>
+      </bao-list-item>
+    </bao-list>
+  `
+});
+SimpleListWithActionButton.storyName = 'Simple list - Action button';
+SimpleListWithActionButton.args = {
   ...Primary.args
 };
